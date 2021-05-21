3d6bb6e9dd9485bf60fb327864f9478a8786493d
Keona Kirby <keonakirby@gmail.com>
2021-05-21 18:07:38 -0500

    set up routes in app.js. Next the pages and components need to be defined

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "../src/components/Nav";
+import Search from "../src/pages/Search";
+import Saved from "../src/pages/Saved";
+import NoMatch from "../src/pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ Client/src/pages/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+
+function Search() {
+  return;
+  <div>
+    <Jumbotron>
+      <h1>React Google Books Search</h1>
+      <h3>Search for and save books of interest</h3>
+    </Jumbotron>
+  </div>;
+}
+
+export default Search;
