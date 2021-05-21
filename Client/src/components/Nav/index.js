b993a6e0d50e137f5e28df29f8d456b54135bd34
Keona Kirby <keonakirby@gmail.com>
2021-05-21 17:33:38 -0500

    customized the navbar

--- Client/src/components/Nav/index.js
+++ Client/src/components/Nav/index.js
@@ -2,10 +2,37 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Google
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Search
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                Saved
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
