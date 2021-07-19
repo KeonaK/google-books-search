ddfbab648b2d8901c5cd6d4ac0c639951267ca58
Keona Kirby <keonakirby@gmail.com>
2021-07-18 22:09:18 -0500

    fixed color of view button

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-success">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           Google
